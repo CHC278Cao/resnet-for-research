882f11e709e74bbd9727d266d0dc25a1fea2a902
Jason Cao <10395876+CHC278Cao@users.noreply.github.com>
2018-11-02 16:31:11 -0400

    Add files via upload

---
+++ model4_main.py
@@ -0,0 +1,192 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+from datetime import datetime
+
+from resnet import Resnet
+from DataUtils import *
+
+# imgfile and labelfile
+ROOT = os.getcwd()
+print("ROOTpath is {}".format(ROOT))
+filepath = os.path.abspath(os.path.join(ROOT, os.pardir))
+
+train_file = [filepath + "/imgdata/train/oridata/csvfile/train_" + str(i) + ".csv"  for i in range(1, 29)]
+valid_file = [filepath + "/imgdata/valid/oridata/valid_" + str(i) + ".csv" for i in range(1, 4)]
+
+#valid_img_path = filepath + "/imgdata/valid/valid_x.csv"
+#valid_lab_path = filepath + "/imgdata/valid/valid_y.csv"
+
+# filewriter and saver
+filewriter_path = filepath + "/tensorboard"
+checkpoint_path = filepath + "/checkfile"
+datafile_path = filepath + "/result"
+
+if not os.path.isdir(filewriter_path):
+    os.mkdir(filewriter_path)
+if not os.path.isdir(checkpoint_path):
+    os.mkdir(checkpoint_path)
+
+# load image and label files
+train_img, train_label = concat_data(train_file)
+valid_img, valid_label = concat_data(valid_file)
+
+valid_img, valid_label = dataShuffle(valid_img, valid_label, shuffle = True)
+print("train_img.shape is {}, train_label.shape is {}".format(train_img.shape, train_label.shape))
+print("valid_img.shape is {}, valid_label.shape is {}".format(valid_img.shape, valid_label.shape))
+
+# parameters for model
+init_learning_rate = 0.001
+init_momentum = 0.99
+batch_size = 128
+num_iterations = 40000
+num_classes = 2
+size_input = train_img.shape[1]
+imgsize = 129
+decay_steps = 1000
+display_step = 100
+csv_steps = 5000
+
+# Iterator for mini-batch
+trainimage = tf.placeholder(train_img.dtype, train_img.shape)
+trainlabel = tf.placeholder(train_label.dtype, train_label.shape)
+train_dataset = tf.data.Dataset.from_tensor_slices((trainimage, trainlabel)).shuffle(buffer_size = 50000).repeat(20).batch(batch_size)
+train_iter = train_dataset.make_initializable_iterator()
+train_img_batch, train_lab_batch = train_iter.get_next()
+
+validimage = tf.placeholder(valid_img.dtype, valid_img.shape)
+validlabel = tf.placeholder(valid_label.dtype, valid_label.shape)
+valid_dataset = tf.data.Dataset.from_tensor_slices((validimage, validlabel)).shuffle(buffer_size = 30000).repeat().batch(batch_size)
+valid_iter = valid_dataset.make_initializable_iterator()
+valid_img_batch, valid_lab_batch = valid_iter.get_next()
+
+# model
+x = tf.placeholder(tf.float32, [None, size_input])
+y = tf.placeholder(tf.float32, [None, num_classes])
+keep_prob = tf.placeholder(tf.float32)
+train_mode = tf.placeholder(tf.bool)
+
+model = Resnet(x, num_classes, imgsize, train_mode, keep_prob)
+out = model.out
+
+with tf.name_scope("prediction"):
+    pre = tf.argmax(tf.nn.softmax(out), 1)
+
+with tf.name_scope("Loss"):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = out, labels = y))
+    tf.summary.scalar("Loss", loss)
+
+# optimizer
+with tf.name_scope("Optimizer"):
+    global_step = tf.Variable(0, trainable = False)
+    learn_rate = tf.train.exponential_decay(init_learning_rate, global_step, decay_steps, 0.96, staircase = True)
+    momentum = tf.train.exponential_decay(init_momentum, global_step, 2000, 0.9, staircase = True)
+
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        optimizer = tf.train.MomentumOptimizer(learning_rate = learn_rate, momentum = momentum).minimize(loss, global_step = global_step)
+
+with tf.name_scope("Accuracy"):
+    correct_pred = tf.equal(tf.argmax(out, 1), tf.argmax(y, 1))
+    acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    tf.summary.scalar("Accuracy", acc)
+
+
+with tf.name_scope("Matrix"):
+    y_pred = tf.argmax(out, 1)
+    y_actual = tf.argmax(y, 1)
+    TP = tf.count_nonzero(y_pred * y_actual)
+    TN = tf.count_nonzero((y_pred - 1) * (y_actual - 1))
+    FP = tf.count_nonzero(y_pred * (y_actual - 1))
+    FN = tf.count_nonzero((y_pred - 1) * y_actual)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * precision * recall / (precision + recall)
+    tf.summary.scalar("precision", precision)
+    tf.summary.scalar("recall", recall)
+    tf.summary.scalar("f1", f1)
+
+
+init = tf.global_variables_initializer()
+merge_summary_op = tf.summary.merge_all()
+data_dict = {"Iter": [], "train_loss": [], "train_acc": [], "train_pre": [], "train_recall": [], "train_f1": [],
+            "valid_loss": [], "valid_acc": [], "valid_pre": [], "valid_recall": [], "valid_f1": []}
+pre_dict = {"label": [], "pre": []}
+train_writer = tf.summary.FileWriter(filewriter_path + "/train")
+valid_writer = tf.summary.FileWriter(filewriter_path + "/test")
+saver_step = 4000
+with tf.Session() as sess:
+    sess.run(init)
+    train_writer.add_graph(sess.graph)
+    saver = tf.train.Saver()
+    global_step.eval()
+    sess.run(train_iter.initializer, feed_dict = {trainimage: train_img, trainlabel: train_label})
+    sess.run(valid_iter.initializer, feed_dict = {validimage: valid_img, validlabel: valid_label})
+
+#    ckpt = tf.train.get_checkpoint_state(os.path.dirname(checkpoint_path))
+#    if ckpt and ckpt.model_checkpoint_path:
+#        saver.restore(sess, ckpt.model_checkpoint_path)
+#    else:
+#        pass
+
+    for itr in range(num_iterations):
+        global_step = itr
+        train_imgbatch, train_labatch = sess.run([train_img_batch, train_lab_batch])
+        feed_dict = {x: train_imgbatch, y: train_labatch, keep_prob: 0.5, train_mode: True}
+        sess.run(optimizer, feed_dict = feed_dict)
+
+
+        if itr % 10 == 0:
+            trs = sess.run(merge_summary_op, feed_dict = feed_dict)
+            train_writer.add_summary(trs, itr)
+            valid_imgbatch, valid_labatch = sess.run([valid_img_batch, valid_lab_batch])
+            vrs = sess.run(merge_summary_op, feed_dict = {x: valid_imgbatch, y: valid_labatch, keep_prob: 1.0, train_mode: False})
+            valid_writer.add_summary(vrs, itr)
+
+
+        if itr % display_step == 0:
+            train_imgbatch, train_labatch = sess.run([train_img_batch, train_lab_batch])
+            tl, tac, tpre, trec, tfvalue = sess.run([loss, acc, precision, recall, f1], feed_dict = {x: train_imgbatch, y: train_labatch, keep_prob: 0.5, train_mode: True})
+            valid_imgbatch, valid_labatch = sess.run([valid_img_batch, valid_lab_batch])
+            vl, vac, vpre, vrec, vfvalue, aculabel = sess.run([loss, acc, precision, recall, f1, pre], feed_dict = {x: valid_imgbatch, y: valid_labatch, keep_prob: 1.0, train_mode: False})
+
+            print("-----------Iteration {}-----------".format(itr))
+            print("-----------{} starting displaying -----------".format(datetime.now()))
+            print("Training Loss is {}, Accuracy is {}".format(tl, tac))
+            print("Validing Loss is {}, Accuracy is {}".format(vl, vac))
+
+            data_dict["Iter"].append(itr)
+            data_dict["train_loss"].append(tl)
+            data_dict["train_acc"].append(tac)
+            data_dict["train_pre"].append(tpre)
+            data_dict["train_recall"].append(trec)
+            data_dict["train_f1"].append(tfvalue)
+            data_dict["valid_loss"].append(vl)
+            data_dict["valid_acc"].append(vac)
+            data_dict["valid_pre"].append(vpre)
+            data_dict["valid_recall"].append(vrec)
+            data_dict["valid_f1"].append(vfvalue)
+            pre_dict["label"].append(sess.run(tf.argmax(valid_labatch, axis = 1)))
+            pre_dict["pre"].append(aculabel)
+
+        if itr % saver_step == 0:
+            saver.save(sess, checkpoint_path + "/model3_r2_Iter_" + str(itr+1)+ ".ckpt",
+                    global_step = global_step)
+
+        if itr % csv_steps == 0:
+            data_df = pd.DataFrame(data_dict)
+            data_df.to_csv(datafile_path + "/model3_r2_" + str(itr) + ".csv")
+            pre_df = pd.DataFrame(pre_dict)
+            pre_df.to_csv(datafile_path + "/model3_r2_pre_" + str(itr) + ".csv")
+
+
+    print("------------------finished---------------------")
+
--- resnet.py
+++ resnet.py
@@ -4,73 +4,110 @@
 # In[ ]:
 
 
-from resnetUtils import *
-import numpy as np
+from netUtils import *
 import tensorflow as tf
+import numpy as np
 
 class Resnet(object):
-    def __init__(self, inputs, num_classes, imgsize, is_training):
+    def __init__(self, inputs, num_classes, imgsize, is_training, keep_prob):
         self.inputs = tf.reshape(inputs, [-1, imgsize, imgsize, 1])
         self.num_classes = num_classes
         self.is_training = is_training
-        
-        self._buildModel(self.is_training)
-        
-    def _buildModel(self, train_mode):
+        self.keep_prob = keep_prob
+
+        self._buildModel(self.is_training, self.keep_prob)
+
+    def _buildModel(self, train_mode, keep_prob):
         # inputs is 129x129x1
         with tf.variable_scope("conv0"):
             conv0 = conv_bn_relu_layer(self.inputs, 8, 3, 1, padding = "VALID", is_training = train_mode)
             activation_summary(conv0)
-            
+
         # feature map is 127x127x8
         with tf.variable_scope("pool0"):
             pool0 = pool2d(conv0, ksize = 2, stride = 2)
             activation_summary(pool0)
-        
+
         # feature map is 64x64x8
         with tf.variable_scope("conv1"):
             conv1 = block_v1(pool0, 8, 8, 16, train_mode)
             activation_summary(conv1)
-        
+
         # feature map is 64x64x16
         with tf.variable_scope("conv2"):
             conv2 = resdual_v1(conv1, 16, 16, 16, train_mode)
             activation_summary(conv2)
-            
-        # feature map is 64x64x16
+
+        # feature map is 64x64x32
         with tf.variable_scope("conv3"):
             conv3 = resdual_v2(conv2, 16, 16, 32, train_mode)
             activation_summary(conv3)
-        
+
         # feature map is 32x32x32
         with tf.variable_scope("conv4"):
             conv4 = resdual_v1(conv3, 16, 16, 32, train_mode)
             activation_summary(conv4)
-            
-        # feature map is 32x32x32
+
+        # feature map is 32x32x64
         with tf.variable_scope("conv5"):
             conv5 = resdual_v2(conv4, 16, 16, 64, train_mode)
             activation_summary(conv5)
-        
+
         # feature map is 16x16x64
         with tf.variable_scope("conv6"):
             conv6 = resdual_v1(conv5, 32, 32, 64, train_mode)
             activation_summary(conv6)
-        
+
         # feature map is 16x16x64
         with tf.variable_scope("conv7"):
             conv7 = resdual_v2(conv6, 32, 32, 64, train_mode)
             activation_summary(conv7)
-        
+
+        # feature map is 16x16x64
+        with tf.variable_scope("conv8"):
+            conv8 = resdual_v1(conv7, 32, 32, 64, train_mode)
+            activation_summary(conv8)
+
+        # feature map is 16x16x64
+        with tf.variable_scope("conv9"):
+            conv9 = resdual_v2(conv8, 32, 32, 64, train_mode)
+            activation_summary(conv9)
+
         # feature map is 8x8x64
-        with tf.variable_scope("pool8"):
-            pool8 = pool2d(conv7, ksize = 8, stride = 1, padding = "VALID", active_func = tf.nn.avg_pool)
-        
-        # feature map is 1x1x64
-        with tf.variable_scope("fc"):
-            in_dim = pool8.get_shape().as_list()[-1]
-            global_pool = tf.reduce_mean(pool8, [1, 2])
-            output = output_layer(global_pool, self.num_classes)
-        
-        self.out = output
+        with tf.variable_scope("conv10"):
+            w = create_conv_variables(name = "weights", shape = [1, 1, 64, 64])
+            b = create_bias_variables(name = "biases", shape = [64])
+            conv10 = conv2d(conv9, w, b, stride = 1 , padding = "SAME")
+            activation_summary(conv10)
+
+        # feature map is 8x8x64
+        shape = conv10.get_shape().as_list()
+        flatten  = tf.reshape(conv10, shape = [-1, shape[1]*shape[2]*shape[3]])
+
+        with tf.variable_scope("fc1"):
+            fc1 = fc_layer(flatten, 500)
+            fc1 = tf.cond(train_mode, lambda: tf.nn.dropout(fc1, keep_prob), lambda: fc1)
+            activation_summary(fc1)
+
+        with tf.variable_scope("fc2"):
+            fc2 = fc_layer(fc1, 500)
+            fc2 = tf.cond(train_mode, lambda: tf.nn.dropout(fc2, keep_prob), lambda: fc2)
+            activation_summary(fc2)
+
+        with tf.variable_scope("fc3"):
+            fc3 = fc_layer(fc2, 200)
+            fc3 = tf.cond(train_mode, lambda: tf.nn.dropout(fc3, keep_prob), lambda: fc3)
+            activation_summary(fc3)
+
+        with tf.variable_scope("fc4"):
+            fc4 = fc_layer(fc3, 200)
+            fc4 = tf.cond(train_mode, lambda: tf.nn.dropout(fc4, keep_prob), lambda: fc4)
+            activation_summary(fc4)
+
+        with tf.variable_scope("fc5"):
+            fc5 = fc_layer(fc4, self.num_classes)
+            fc5 = tf.cond(train_mode, lambda: tf.nn.dropout(fc5, keep_prob), lambda: fc5)
+            activation_summary(fc5)
+
+        self.out = fc5
 
