82d547ea611e94fd6de391f808812557b94360f4
Jason Cao <10395876+CHC278Cao@users.noreply.github.com>
2018-11-02 16:27:17 -0400

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -14,6 +14,8 @@ class Resnet(object):
         self.num_classes = num_classes
         self.is_training = is_training
         
+        self._buildModel(self.is_training)
+        
     def _buildModel(self, train_mode):
         # inputs is 129x129x1
         with tf.variable_scope("conv0"):
@@ -27,37 +29,37 @@ class Resnet(object):
         
         # feature map is 64x64x8
         with tf.variable_scope("conv1"):
-            conv1 = block_v1(pool0, 8, 8, 16, self.is_training)
+            conv1 = block_v1(pool0, 8, 8, 16, train_mode)
             activation_summary(conv1)
         
         # feature map is 64x64x16
         with tf.variable_scope("conv2"):
-            conv2 = resdual_v1(conv1, 8, 8, 16, self.is_training)
+            conv2 = resdual_v1(conv1, 16, 16, 16, train_mode)
             activation_summary(conv2)
             
-        # feature map is 64x64x32
+        # feature map is 64x64x16
         with tf.variable_scope("conv3"):
-            conv3 = resdual_v2(conv2, 8, 8, 32, self.is_training)
+            conv3 = resdual_v2(conv2, 16, 16, 32, train_mode)
             activation_summary(conv3)
         
         # feature map is 32x32x32
         with tf.variable_scope("conv4"):
-            conv4 = resdual_v1(conv3, 16, 16, 32, self.is_training)
+            conv4 = resdual_v1(conv3, 16, 16, 32, train_mode)
             activation_summary(conv4)
             
-        # feature map is 32x32x64
+        # feature map is 32x32x32
         with tf.variable_scope("conv5"):
-            conv5 = resdual_v2(conv4, 16, 2, 64, self.is_training)
+            conv5 = resdual_v2(conv4, 16, 16, 64, train_mode)
             activation_summary(conv5)
         
         # feature map is 16x16x64
         with tf.variable_scope("conv6"):
-            conv6 = resdual_v1(conv5, 32, 32, 64, self.is_training)
+            conv6 = resdual_v1(conv5, 32, 32, 64, train_mode)
             activation_summary(conv6)
         
         # feature map is 16x16x64
         with tf.variable_scope("conv7"):
-            conv7 = resdual_v2(conv6, 64, 64, 64, self.is_training)
+            conv7 = resdual_v2(conv6, 32, 32, 64, train_mode)
             activation_summary(conv7)
         
         # feature map is 8x8x64
